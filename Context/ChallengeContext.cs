f33c8732b9320c9d2647509ca2cff0815a502eab
tavolafourcade <tavolafourcade@gmail.com>
2021-07-27 19:59:18 -0500

    City, Continent and Geographic Icons entities created

---
+++ Context/ChallengeContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Challenge.PreAceleracion.Context
+{
+    public class ChallengeContext : DbContext
+    {
+        public ChallengeContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Continent> Continents { get; set; } = null;
+
+        public DbSet<City> City { get; set; } = null;
+
+        public DbSet<GeographicIcons> GeographicIcons { get; set; } = null;
+    }
+}
