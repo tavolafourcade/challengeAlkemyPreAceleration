fa004a93c1ccc544977b7e64cd6790a3572e5e3e
tavolafourcade <tavolafourcade@gmail.com>
2021-07-28 17:40:46 -0500

    Generate Entities and Context and Implemented Infraestructura to allow the code how to conect to the DB

--- Entities/GeographicIcons.cs
+++ Entities/GeographicIcons.cs
@@ -7,5 +7,11 @@ namespace Challenge.PreAceleracion.Entities
 {
     public class GeographicIcons
     {
+        public int Id { get; set; }
+        public string Image { get; set; }
+        public DateTime CreationDate { get; set; }
+        public int Height { get; set; }
+        public string History { get; set; }
+
     }
 }
